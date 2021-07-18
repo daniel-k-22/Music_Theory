9181719277bdb6aa855a194c56d59e2ce9525282
Daniel <daniel.raj2299@gmail.com>
2021-07-18 11:47:49 +0530

    Added Suspened Chords

--- Chord.py
+++ Chord.py
@@ -13,9 +13,19 @@ class Chord_Structure:
         scale_result = scale.Scale(self.key, self.scale_type)
         if self.chord_type == "triad":
             result = self.Triad(scale_result)
+        if self.chord_type == "sus2":
+            result = self.Sus2(scale_result)
+        if self.chord_type == "sus4":
+            result = self.Sus4(scale_result)
         return(result)
 
     def Triad(self, scale): # 1-3-5 chord
         chord = scale[0] + "-" + scale[2] + "-" + scale[4]
+        return(chord)
+    def Sus2(self, scale): # 1-2-5 chord
+        chord = scale[0] + "-" + scale[1] + "-" + scale[4]
+        return(chord)
+    def Sus4(self, scale): # 1-2-5 chord
+        chord = scale[0] + "-" + scale[3] + "-" + scale[4]
         return(chord)
         
